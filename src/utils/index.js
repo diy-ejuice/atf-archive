e0e98dc8c1de2fe45d04e5796435c6a844f43fd0
ayan4m1 <andrew@bulletlogic.com>
2020-12-10 12:00:46 -0500

    trim recipe name slugs

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,7 +8,12 @@ const getFlavorSlug = (flavor) =>
   getSlug(`/flavor/${flavor.id} ${flavor.vendor} ${flavor.name}`);
 
 const getRecipeSlug = (recipe) =>
-  getSlug(`/recipe/${recipe.id} ${recipe.name}`);
+  getSlug(
+    `/recipe/${recipe.id} ${recipe.name.substring(
+      0,
+      Math.min(50, recipe.name.length)
+    )}`
+  );
 
 const getMixerSlug = (mixer) => getSlug(`/mixer/${mixer.name}`);
 
