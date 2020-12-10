5fb0574472d23a845d51892e53967c73a61f2ab3
ayan4m1 <andrew@bulletlogic.com>
2020-12-10 00:26:40 -0500

    implement simple flavors list

--- src/pages/flavors.js
+++ src/pages/flavors.js
@@ -1,16 +1,55 @@
+import { graphql, Link } from 'gatsby';
+import PropTypes from 'prop-types';
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 
 import Layout from '~components/Layout';
 import SEO from '~components/SEO';
+import { getFlavorSlug } from '~utils';
 
-export default function FlavorsPage() {
+export default function FlavorsPage({ data }) {
+  const flavors = data.allFlavorsJson.nodes;
+
   return (
     <Layout>
       <SEO title="Flavors" />
       <Container>
         <h1>Flavors</h1>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Vendor</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {flavors.map((flavor) => (
+              <tr key={flavor.id}>
+                <td>{flavor.vendor}</td>
+                <td>
+                  <Link to={getFlavorSlug(flavor)}>{flavor.name}</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </Container>
     </Layout>
   );
 }
+
+FlavorsPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export const pageQuery = graphql`
+  query {
+    allFlavorsJson {
+      nodes {
+        id
+        name
+        vendor
+      }
+    }
+  }
+`;
