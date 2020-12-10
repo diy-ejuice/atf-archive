1810054887c54b034c10893a760fd4ee54d3dc2c
ayan4m1 <andrew@bulletlogic.com>
2020-12-10 00:27:45 -0500

    search recipes by name or author and switch to react-window for performance

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,42 +1,74 @@
+import { FixedSizeList as List } from 'react-window';
 import { graphql, Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { Container, Table } from 'react-bootstrap';
+import React, { useCallback, useState } from 'react';
+import { Container, Card, FormControl } from 'react-bootstrap';
 
 import Layout from '~components/Layout';
 import SEO from '~components/SEO';
 import { getRecipeSlug } from '~utils';
+import { debounce } from 'lodash';
 
 export default function RecipesPage({ data }) {
   const recipes = data.allRecipesJson.nodes;
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSetSearchTerm = debounce(setSearchTerm, 250);
+  const handleChangeTerm = useCallback(
+    (event) => debouncedSetSearchTerm(event.target.value),
+    [debouncedSetSearchTerm]
+  );
+
+  const filteredRecipes = recipes.filter((recipe) => {
+    const searchExpr = new RegExp(searchTerm, 'i');
+
+    return (
+      searchTerm === '' ||
+      searchExpr.test(recipe.name) ||
+      searchExpr.test(recipe.author)
+    );
+  });
+
+  filteredRecipes.sort((a, b) => b.views - a.views);
+
+  function RecipeRow({ index, style }) {
+    const recipe = filteredRecipes[index];
+
+    return (
+      <Card key={recipe.id} style={style}>
+        <Card.Header>
+          <Card.Title>
+            <Link to={getRecipeSlug(recipe)}>
+              {recipe.name} by {recipe.author}
+            </Link>
+          </Card.Title>
+        </Card.Header>
+      </Card>
+    );
+  }
+
+  RecipeRow.propTypes = {
+    index: PropTypes.number.isRequired,
+    style: PropTypes.object
+  };
 
   return (
     <Layout>
       <SEO title="Recipes" />
       <Container>
         <h1>Recipes</h1>
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Author</th>
-              <th>Created</th>
-              <th>Views</th>
-            </tr>
-          </thead>
-          <tbody>
-            {recipes.map((recipe) => (
-              <tr key={recipe.id}>
-                <td>
-                  <Link to={getRecipeSlug(recipe)}>{recipe.name}</Link>
-                </td>
-                <td>{recipe.author}</td>
-                <td></td>
-                <td>{recipe.views}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        <FormControl
+          type="text"
+          onChange={handleChangeTerm}
+          placeholder="search by name or author"
+        />
+        <List
+          height={800}
+          itemData={filteredRecipes}
+          itemCount={filteredRecipes.length}
+          itemSize={60}
+        >
+          {RecipeRow}
+        </List>
       </Container>
     </Layout>
   );
